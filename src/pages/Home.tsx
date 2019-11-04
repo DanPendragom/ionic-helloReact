647c7f87932dedbdb9e37c10e7acf602702d39f4
DanPendragom <carlos.coutinho5@etec.sp.gov.br>
2019-11-04 14:29:16 -0300

    First Commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,14 +10,7 @@ const Home: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-        The world is your oyster.
-        <p>
-          If you get lost, the{' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
-            docs
-          </a>{' '}
-          will be your guide.
-        </p>
+        <p>Primeira aplicação react</p>
       </IonContent>
     </IonPage>
   );
