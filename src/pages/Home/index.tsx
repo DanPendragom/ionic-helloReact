09f6f1511f2d45c4bc463a84b7ea4a717793a70b
Daniel Pendragon <carlos.coutinho5@etec.sp.gov.br>
2019-11-08 09:27:40 -0300

    Função Calcular

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,19 @@
 import { IonContent, IonGrid, IonItem, IonLabel, IonInput, IonRow, IonCol, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
 import React, {Component} from 'react';
+import App from '../../App';
 
 export default class Home extends Component{
   
-  getvalue(){
+  calcular(v:number, v2:number){
+    let res = v + v2;
+    document.querySelectorAll('input')[2].value = res.toString();
+  }
+
+  pegaValores(){
+    let v1 = parseInt(document.querySelectorAll('input')[0].value);
+    let v2 = parseInt(document.querySelectorAll('input')[1].value);
+
+    this.calcular(v1, v2);
   }
   
   render(){
@@ -12,7 +22,7 @@ export default class Home extends Component{
   
         <IonHeader>
           <IonToolbar>
-            <IonTitle> Bem Vindo</IonTitle>
+            <IonTitle>Bem Vindo</IonTitle>
           </IonToolbar>
         </IonHeader>
   
@@ -23,7 +33,7 @@ export default class Home extends Component{
               <IonCol size="12">
                 <IonItem>
                   <IonLabel position="floating">Valor 1</IonLabel>
-                  <IonInput  type="number"></IonInput>
+                  <IonInput id="valor1" value="" type="number"></IonInput>
                 </IonItem>
               </IonCol>
             </IonRow>
@@ -39,10 +49,18 @@ export default class Home extends Component{
   
             <IonRow>
               <IonCol size="12">
-                <IonButton onClick={this.getvalue} expand="block">Calcular</IonButton>
+                <IonButton onClick={() =>this.pegaValores()} expand="block">Calcular</IonButton>
               </IonCol>
             </IonRow>
-  
+
+            <IonRow>
+              <IonCol>
+                <IonItem>
+                  <IonInput placeholder="resultado" readonly type="number"></IonInput>
+                </IonItem>
+              </IonCol>
+            </IonRow>
+
           </IonGrid>
         </IonContent>
       </IonPage>
